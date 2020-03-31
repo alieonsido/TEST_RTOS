e8ae6155357596815849020ace22de944b7fc062
alieon <alieonsido@gmail.com>
2020-04-01 03:06:04 +0800

    add Getting RM scheduling statistics code

---
+++ Lab1/periodic task set and RM scheduling/t1rm.cpp
@@ -0,0 +1,82 @@
+//#define _GNU_SOURCE
+#include <sched.h>
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include <cmath>
+#include <unistd.h>
+#include <signal.h>
+
+// sig_atomic_t t=0;
+
+#define CPU_CORE 0
+#define WORKLOADTIME 2000
+#define PERIODTIME 5000
+#define PRIORITY 99
+
+void setSchedulingPolicy (int policy, int priority)
+{
+    sched_param sched;
+    sched_getparam(0, &sched);
+    sched.sched_priority = priority;
+    if (sched_setscheduler(0, policy, &sched)) {
+        perror("sched_setscheduler");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void workload_1ms (void)
+{
+	int repeat = 625; // tune this for the right amount of workload
+	int x = 0;
+	for (int i = 0; i <= repeat; i++)
+	{
+		// add some computation here (e.g., use sqrt() in cmath)
+		x = sqrt(2);
+	}
+}
+
+void pinCPU (int cpu_number)
+{
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+
+    CPU_SET(cpu_number, &mask);
+
+    if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) == -1)
+    {
+        perror("sched_setaffinity");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main (void)
+{
+
+    // printf("Choose your CPU: ");
+    // scanf("%d",&cpu_core);
+    pinCPU(CPU_CORE);
+    int period = PERIODTIME; // unit: microsecond
+    int delta;
+    setSchedulingPolicy (SCHED_FIFO, PRIORITY);
+    while (1)
+    {
+        std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+        for (int j = 0; j < WORKLOADTIME; j++)
+        {
+            workload_1ms ();
+        }
+        std::chrono::system_clock::time_point endTime = std::chrono::system_clock::now();
+        delta = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+        //::t++;
+        if (delta > period)
+        {
+            continue;
+        }
+        else
+        {
+            usleep (period-delta);
+        }
+    }
+    return 0;
+}
